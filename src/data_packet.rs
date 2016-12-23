eb096dfeb11875ca94d76d60825d9970cc63354e
Valentin Lorentz <progval+git@progval.net>
2016-12-23 14:16:51 +0100

    Add data packet constructor.

--- src/data_packet.rs
+++ src/data_packet.rs
@@ -18,8 +18,20 @@ pub struct DataPacket {
 }
 
 impl DataPacket {
+    pub fn new(version: u8, payload: &Payload) -> DataPacket {
+        assert!(version <= 0b1111);
+        let mut raw = vec![version << 4, 0, 0, 0];
+        match *payload {
+            Payload::RoutePacket(ref route_packet) => {
+                BigEndian::write_u16(&mut raw[2..4], 256);
+                let encoded_route_packet = route_packet.clone().encode(); // TODO: do not copy
+                raw.extend(encoded_route_packet) // TODO: do not copy
+            }
+        }
+        DataPacket { raw: raw }
+    }
     pub fn version(&self) -> u8 {
-        self.raw[0] >> 3
+        self.raw[0] >> 4
     }
 
     pub fn unused1(&self) -> u8 {
@@ -50,6 +62,6 @@ impl DataPacket {
 
 impl fmt::Display for DataPacket {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "DataPacket(version={}, content_type={}, payload={:?})", self.version(), self.content_type(), self.clone().payload())
+        write!(f, "DataPacket(version={}, payload={:?})", self.version(), self.clone().payload())
     }
 }
